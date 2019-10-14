49e5878bb7e5121bbae59f8895ba317bace92500
”Ozi <31310132+OziOcb@users.noreply.github.com>
2019-10-15 00:23:08 +0100

    Install GSAP & create the Parallax Section

--- js/main.js
+++ js/main.js
@@ -1,9 +1,9 @@
 let counter = 1;
 const controller = new ScrollMagic.Controller({addIndicators: true});
 
+// users fading ing
 const users = document.querySelectorAll('.user');
 users.forEach(user => {
-	console.log('user -->', user);
 	new ScrollMagic.Scene({
 		triggerElement: user,
 		triggerHook: 0.6,
@@ -17,3 +17,22 @@ users.forEach(user => {
 		.setClassToggle(user, "show")
 		.addTo(controller);
 })
+
+// parallax section
+const parallaxTimeLine = new TimelineMax();
+parallaxTimeLine
+	.from('.sectionParallax__content', .3, {autoAlpha: 0, ease:Power0.easeNone}, .8)
+	.from('.sectionParallax__bg', 2, {y: '-25%', ease:Power0.easeNone}, 0);
+
+const sectionParallaxScene = new ScrollMagic.Scene({
+	triggerElement: '.sectionParallax',
+	triggerHook: 1,
+	duration: '100%'
+})
+	.addIndicators({
+		name: `parallax`,
+		colorEnd: "#b00b55",
+		colorTrigger: "#ddd"
+	})
+	.setTween(parallaxTimeLine)
+	.addTo(controller)
